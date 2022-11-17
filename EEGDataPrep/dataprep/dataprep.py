3c36138829a4819e5262857a1acc8ab7ca85d2cf
yulilev <yuvalevy278@gmail.com>
2022-11-17 15:02:48 +0200

    first step completed

--- EEGDataPrep/dataprep/dataprep.py
+++ EEGDataPrep/dataprep/dataprep.py
@@ -1,9 +1,39 @@
 import os
 import mne
 
+
 # Plot raw data
 def plot_raw(file):
     raw = mne.io.read_raw(file, preload=True)
     # Deleting extra channels for development sake. DELETE AFTER FINISHED
     raw.pick(['Pz', 'Cz', 'Fz', 'C3', 'C4'])
     raw.plot(duration=4)
+
+
+'''bendpass (elctricity, noice...), lowpass= the high limit, highpass=the low limit.
+    we are going to cancel them'''
+
+
+def step1(file):
+    #
+    raw = mne.io.read_raw(file, preload=True)
+    # Deleting extra channels for development sake. DELETE AFTER FINISHED
+    raw.pick(['Pz', 'Cz', 'Fz', 'C3', 'C4'])
+    bandpass_filter(raw)
+    rereference(raw)
+    print("---------------------------------")
+    print("Pick bad channels")
+    raw.plot(duration=4)
+    return raw
+
+
+def bandpass_filter(raw):
+    raw.filter(l_freq=1, h_freq=40)  # low&high
+    raw.resample(sfreq=250)  # מדלל
+    # Cleaning powerline noise
+    freqs = (60, 120, 180, 240)
+    raw.notch_filter(freqs=freqs, picks='eeg', method='spectrum_fit', filter_length='4s')
+
+
+def rereference(raw):
+    raw.set_eeg_reference(ref_channels=['Cz'])
--- EEGDataPrep/front/front.py
+++ EEGDataPrep/front/front.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter.filedialog import askopenfilename
 from EEGDataPrep.dataprep.dataprep import *
 
+
 # Class of software window
 # We can create as many windows as we wish
 # You can create instances through runner.py
@@ -10,19 +11,23 @@ class MyWindow:
     def __init__(self, win):
         # Creating the visuals
         # ---------------------------
-        # Labels-טקסטים
+        # Labels-texts
         self.lblTitle = Label(win, font="Helvetica 16 bold", text='BRAINPUT - EEG Raw data Processor')
         self.lblSubtitle = Label(win, text='Choose your EEG data')
         self.lblBtnLeft = Label(win, font="Helvetica 14 bold", text='Create plots from file')
         self.lblBtnRight = Label(win, font="Helvetica 14 bold", text='Process Your File')
-
+        self.lblStep1 = Label(win, text='Bandpass, re-referencing & Channel inspection')
+        self.lblStep2 = Label(win, text='Removing bad channels, epoching, inspecting epochs,')
+        self.lblStep3 = Label(win, text='ICA & Save processed file')
         # Buttons
         self.btnOpenFile = Button(win, text='Load File', command=self.select_file)
         self.btnRawPlot = Button(win, text='Plot Raw', command=self.create_plot)
-
+        self.btnStep1 = Button(win, text='Step One', command=self.first_step)
+        self.btnStep2 = Button(win, text='Step Two', command=self.second_step)
         # EEG file loaded by user
         # ---------------------------
         self.selectedFile = "No file loaded"
+        self.midProcess = mne.create_info(4, sfreq=40)
 
         # Placing the objects
         # ---------------------------
@@ -31,10 +36,14 @@ class MyWindow:
         self.lblSubtitle.place(x=20, y=90)
         self.lblBtnLeft.place(x=20, y=140)
         self.lblBtnRight.place(x=190, y=140)
+        self.lblStep1.place(x=250, y=180)
+        self.lblStep2.place(x=250, y=220)
 
         # Buttons
         self.btnOpenFile.place(x=170, y=90)
         self.btnRawPlot.place(x=20, y=180)
+        self.btnStep1.place(x=190, y=180)
+        self.btnStep2.place(x=190, y=220)
 
     # Triggered when Load File is pressed
     def select_file(self):
@@ -62,12 +71,19 @@ class MyWindow:
             ('Nihon Kohden EEG data files', '*.21e'),
             ('XDF EEG data files', '*.xdf'),
             ('XDF EEG data files', '*.xdfz'),
-            ('Elekta NeuroMag data files', '*.fif') #save date in fif
+            ('Elekta NeuroMag data files', '*.fif')  # save date in fif
         )
 
-        self.selectedFile = askopenfilename(filetypes= filetypes, title= 'Open a file')
-
+        self.selectedFile = askopenfilename(filetypes=filetypes, title='Open a file')
 
     # Triggered when Plot Raw is pressed
     def create_plot(self):
         plot_raw(self.selectedFile)
+
+    # Triggered when Step One is pressed
+    def first_step(self):
+        self.midProcess = step1(self.selectedFile)
+
+    # Triggered when Step Two is pressed
+    def second_step(self):
+        print("good job")
